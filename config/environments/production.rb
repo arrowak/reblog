b98978321a4155da98efe250d2b97b8bdd098d82
arun.kolhapur <arun.kolhapur@verse.in>
2018-05-04 16:54:08 +0530

    heroku test change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,21 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  # Don't care if the mailer can't send.
+    config.action_mailer.raise_delivery_errors = false
+
+    config.action_mailer.perform_caching = false
+    config.action_mailer.default_url_options = { :host => 'https://salty-woodland-44498.herokuapp.com' }
+
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+        :address              => "smtp.gmail.com",
+        :port                 => 587,
+        :user_name            => 'arunko350@gmail.com',
+        :password             => 'RcR@240064',
+        :authentication       => 'plain',
+        :enable_starttls_auto => true  }
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
