fb87f1f8f87d7574fce829a9de6bc0365630f390
arun.kolhapur <arun.kolhapur@verse.in>
2018-05-22 16:19:34 +0530

    pagination for posts and made posts public

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,10 @@
 class HomeController < ApplicationController
-  before_action :authenticate_user!
 
   def index
     @activities = PublicActivity::Activity.order("created_at DESC")
   end
 
   def posts
-    @posts = Post.all.order("created_at DESC")
+    @posts = Post.paginate(:page => params[:page], :per_page => 1).order("created_at DESC")
   end
 end
